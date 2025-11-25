4357d55b83f1700ba9b33a84dfa3365440873719
Yun0don <127651725+Yun0don@users.noreply.github.com>
2025-11-25 15:06:00 +0900

    개발환경 세팅 및 깃이그노어 포함 첫 커밋

---
+++ Assets/3.Script/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector3 = System.Numerics.Vector3;
+
+public class PlayerController : MonoBehaviour
+{
+  // [SerializeField] InputReader input;
+  // [SerializeField] float moveSpeed = 5f;
+
+  // void Update()
+  // {
+  //    Vector2 move = input.Move;
+  //    Vector3 dir = new Vector3(move.x, 0, move.y);
+
+  //    transform.Translate(dir * moveSpeed * Time.deltaTime);
+  // }
+}
